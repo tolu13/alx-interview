2adc0097d5083933d61396fafd12e9c673fea288
tolu13 <tolulopeolanrewaju8@gmail.com>
2024-07-05 04:05:04 +0000

    ok

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" Prime Game Algorithm Python """
+
+
+def is_prime(n):
+    """ Checks if a number given n is a prime number """
+    for i in range(2, int(n ** 0.5) + 1):
+        if not n % i:
+            return False
+    return True
