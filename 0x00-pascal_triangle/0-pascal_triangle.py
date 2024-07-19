a81ba750db72f9ba4ca9c67e380cbb2b9b3c6225
tolu13 <tolulopeolanrewaju8@gmail.com>
2024-07-19 02:36:50 +0000

    updated

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
+"""Pascal triangle"""
+
 
 def pascal_triangle(n):
+    """Returns a list of lists of integers 
+    representing the Pascal’s triangle of n:
     """
-    Generate Pascal's triangle up to the nth row.
 
-    Args:
-    - n (int): The number of rows to generate in Pascal's triangle.
-
-    Returns:
-    - list: A list of lists representing Pascal's triangle.
-
-    Raises:
-    - ValueError: If n is less than or equal to 0.
-
-    Example:
-    >>> pascal_triangle(5)
-    [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
-    """
     if n <= 0:
-        raise ValueError("n must be greater than 0")
+        return []
 
-    triangle = [[1]]
+    
+    """ initialize an empty resulting array """
+    pascal = [[] for idx in range(n)]
 
-    for i in range(1, n):
-        row = [1]
-        prev_row = triangle[-1]
+    for li in range(n):
+        for col in range(li+1):
+            if(col < li):
+                if(col == 0):
+                    """ the first column is always set to 1 """
+                    pascal[li].append(1)
+                else:
+                    pascal[li].append(pascal[li-1][col] + pascal[li-1][col-1])
+            elif(col == li):
+                """ the diagonal is always set to 1 """
+                pascal[li].append(1)
 
-        for j in range(1, i):
-            row.append(prev_row[j - 1] + prev_row[j])
-
-        row.append(1)
-        triangle.append(row)
-
-    return triangle
+    return pascal
